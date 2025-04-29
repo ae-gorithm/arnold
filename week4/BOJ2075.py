f4af48ae5e15e3e9ca4d443c47aa345d3cac61fa
tpgh02 <seaho4588@gmail.com>
2025-04-29 19:58:54 +0900

    [BOJ] 2075/실버2/1시간+

---
+++ week4/BOJ2075.py
@@ -0,0 +1,17 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
+n = int(input())
+hq = []
+for _ in range(n):
+    nums = list(map(int, input().split()))
+    for num in nums:
+        if len(hq) < n:
+            heapq.heappush(hq, num)
+        else:
+            if hq[0] < num:
+                heapq.heappop(hq)
+                heapq.heappush(hq, num)
+
+print(hq[0])
