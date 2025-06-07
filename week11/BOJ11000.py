d4f56078d074e0a41ae33ba08f15f6eb0f1532e9
tpgh02 <seaho4588@gmail.com>
2025-06-07 21:19:22 +0900

    [BOJ] 11000/골드5/1시간

--- week11/BOJ11000.py
+++ week11/BOJ11000.py
@@ -2,34 +2,35 @@ import sys
 input = sys.stdin.readline
 n = int(input())
 
-import heapq
-time = [list(map(int, input().split())) for _ in range(n)]
-time.sort()
+# 우선순위 큐
+# import heapq
+# time = [list(map(int, input().split())) for _ in range(n)]
+# time.sort()
 
-hq = []
-heapq.heappush(hq, time[0][0])
+# hq = []
+# heapq.heappush(hq, time[0][1])
 
-for i in range(1, n):
-    if hq[0] > time[i][0]:
-        heapq.heappush(hq, time[i][1])
-    else:
-        heapq.heappop(hq)
-        heapq.heappush(hq, time[i][1])
+# for i in range(1, n):
+#     if hq[0] > time[i][0]:
+#         heapq.heappush(hq, time[i][1])
+#     else:
+#         heapq.heappop(hq)
+#         heapq.heappush(hq, time[i][1])
 
-print(len(hq))
+# print(len(hq))
 
+# imos법
+t = []
+for _ in range(n):
+    a, b = map(int, input().split())
+    t.append([a, 1])
+    t.append([b, -1])
 
-# t = []
-# for _ in range(n):
-#     a, b = map(int, input().split())
-#     t.append([a, 1])
-#     t.append([b, -1])
+t.sort()
+cnt = 0
+ans = 0
+for loc, val in t:
+    cnt += val
+    ans = max(ans, cnt)
 
-# t.sort()
-# cnt = 0
-# ans = 0
-# for loc, val in t:
-#     cnt += val
-#     ans = max(ans, cnt)
-
-# print(ans)
+print(ans)
