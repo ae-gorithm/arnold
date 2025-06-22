3bd2b73ce8cfe93a1cfa034b38a9a2518c2b5331
tpgh02 <seaho4588@gmail.com>
2025-06-22 17:06:23 +0900

    [BOJ] 14501/실버3/50분

--- week13/BOJ14501.java
+++ week13/BOJ14501.java
@@ -1,6 +1,7 @@
 package week13;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BOJ14501 {
@@ -15,13 +16,14 @@ public class BOJ14501 {
 			st = new StringTokenizer(br.readLine());
 			int index = Integer.parseInt(st.nextToken());
 			int money = Integer.parseInt(st.nextToken());
+			if (i != 0){
+				dp[i] = Math.max(dp[i], dp[i-1]);
+			}
 			if(i+index <= n){
 				dp[i+index] = Math.max(money + dp[i], dp[i+index]);
 			}
 		}
-
-		for(int i = 0; i <= n; i++){
-			System.out.println(dp[i]);
-		}
+		Arrays.sort(dp);
+		System.out.println(dp[n]);
 	}
 }
